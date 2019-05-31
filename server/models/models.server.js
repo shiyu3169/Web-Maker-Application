f6a45a3d7bb12246d620a09d300f1f9aae827e81
Shiyu Wang <shiyu3169@gmail.com>
2019-05-31 09:50:47 -0400

    config mlab

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -5,7 +5,7 @@ if (process.env.MLAB_USERNAME_WEBDEV) {
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += '@ds0000.mlab.com:29004/dasdasds'; // use yours
+  connectionString += '@ds363996.mlab.com:63996/heroku_3lxcssmv'; // use yours
 }
 
 var mongoose = require('mongoose');
